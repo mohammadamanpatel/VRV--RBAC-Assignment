e76aba51d2d70626686432e19e811bc6eb26ddff
Mohd Aman Naim Patel <ap5277478@gmail.com>
2024-11-30 19:23:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", UserRoutes);
 app.use("/api/exam", ExamRoutes);
+app.get("/api/docs", (req, res) => {
+  res.redirect("https://www.postman.com/joint-operations-cosmologist-64352344/workspace/exam-system-endpoints/collection/30730048-f9511483-07ec-40e6-ad90-ae8352cb0c45?action=share&creator=30730048");
+});
 app.listen(process.env.PORT, async () => {
   console.log("Our App is working on " + process.env.PORT);
   await DBConnection();
